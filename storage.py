fb028803657b88f0c03718d8357601b6b5c79248
pchelkanat <pchelkanat@mail.ru>
2018-12-19 01:25:17 +0300

    добавление директорий для хранения

--- storage.py
+++ storage.py
@@ -1,15 +1,14 @@
 import json
 import os
 
-#папка с файлами
-BASEDBPATH = 'data' #папка
-BLOCKFILE = 'blockchain' #файл хранящий содержимое блоков: /data/blockchain.json
-TXFILE = 'transactions' #файл, хранящий содержимое транзакций /data/transactions.json
-ACCOUNTFILE = 'account' #файл, хранящий аккаунты пользоватлей
+# папка с файлами
+BASEDBPATH = 'data'  # папка
+BLOCKFILE = 'blockchain'  # файл хранящий содержимое блоков: /data/blockchain.json
+TXFILE = 'transactions'  # файл, хранящий содержимое транзакций /data/transactions.json
+ACCOUNTFILE = 'account'  # файл, хранящий аккаунты пользоватлей
 
 
 class BaseDB():
-
     filepath = ''
 
     def __init__(self):
@@ -29,7 +28,7 @@ class BaseDB():
         raw = ''
         if not os.path.exists(self.filepath):
             return []
-        with open(self.filepath,'r+') as f:
+        with open(self.filepath, 'r+') as f:
             raw = f.readline()
         if len(raw) > 0:
             data = json.loads(raw)
@@ -39,16 +38,16 @@ class BaseDB():
 
     def write(self, item):
         data = self.read()
-        if isinstance(item,list):
+        if isinstance(item, list):
             data = data + item
         else:
             data.append(item)
-        with open(self.filepath,'w+') as f:
+        with open(self.filepath, 'w+') as f:
             f.write(json.dumps(data))
         return True
 
     def clear(self):
-        with open(self.filepath,'w+') as f:
+        with open(self.filepath, 'w+') as f:
             f.write('')
 
     def hash_insert(self, item):
@@ -60,13 +59,14 @@ class BaseDB():
         if not exists:
             self.write(item)
 
+
 class BlockChainDB(BaseDB):
 
     #
     def set_path(self):
         self.filepath = BLOCKFILE
 
-    #последний блок (используется в майнинге)
+    # последний блок (используется в майнинге)
     def last(self):
         bc = self.read()
         if len(bc) > 0:
@@ -74,7 +74,7 @@ class BlockChainDB(BaseDB):
         else:
             return []
 
-    #поиск по соответствующему хэшу в блоках
+    # поиск по соответствующему хэшу в блоках
     def find(self, hash):
         one = {}
         for item in self.find_all():
@@ -83,18 +83,20 @@ class BlockChainDB(BaseDB):
                 break
         return one
 
-    #добавление
+    # добавление
     def insert(self, item):
         self.hash_insert(item.__dict__)
+
 
 class TransactionDB(BaseDB):
     """
     """
+
     #
     def set_path(self):
         self.filepath = TXFILE
 
-    #поиск по соответсвующему хэшу в транзакциях
+    # поиск по соответсвующему хэшу в транзакциях
     def find(self, hash):
         one = {}
         for item in self.find_all():
@@ -103,20 +105,19 @@ class TransactionDB(BaseDB):
                 break
         return one
 
-    #добавление
+    # добавление
     def insert(self, transactions):
         if not isinstance(transactions, list):
             transactions = [transactions]
         for transaction in transactions:
             self.hash_insert(transaction.__dict__)
 
+
 class AccountDB(BaseDB):
     def set_path(self):
         self.filepath = ACCOUNTFILE
 
-    #поиск по аккаунту, возвращает словарь
+    # поиск по аккаунту, возвращает словарь
     def get_account(self):
         ac = self.read()
         return ac[0]
-
-
