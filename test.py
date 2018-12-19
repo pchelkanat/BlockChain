cbebf98eee2b1ef8aa964827c921081b847e969b
pchelkanat <pchelkanat@mail.ru>
2018-12-20 00:20:29 +0300

    test

---
+++ test.py
@@ -0,0 +1,14 @@
+from storage import BlockChainDB, TransactionDB
+
+d={"n":0, "hash":12}
+
+c={"b":1, "hash":13}
+a=[{"b":2, "hash":13},{"n":0, "hash":12}]
+
+b = b'gjahskfjas'
+
+#print(b, type(b), str(b)[2:-1], type(str(b)[2:-1]))
+#TransactionDB().insert(a)
+#temp= TransactionDB().find_all()
+#print(temp)
+#print(temp.___len___())
